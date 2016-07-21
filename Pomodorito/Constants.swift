cd13508779bb282a0341f2d5d45fe1f65cf5b52d
Robin Malhotra <me@rmalhotra.com>
2016-07-21 21:54:18 +0530

    basic timer working now

---
+++ Pomodorito/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Pomodorito
+//
+//  Created by Robin on 21/07/16.
+//  Copyright © 2016 comicsanshq. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct BarColors {
+	static let CompletedColor = UIColor.blueColor().colorWithAlphaComponent(0.6)
+	static let notCompletedColor = UIColor.redColor().colorWithAlphaComponent(0.6)
+}
--- Pomodorito/ViewController.swift
+++ Pomodorito/ViewController.swift
@@ -2,24 +2,65 @@
 //  ViewController.swift
 //  Pomodorito
 //
-//  Created by Vmock on 21/07/16.
+//  Created by Robin on 21/07/16.
 //  Copyright © 2016 comicsanshq. All rights reserved.
 //
 
 import UIKit
-
+import SwiftCharts
 class ViewController: UIViewController {
-
+	
+	var chart: Chart?
+	
+	@IBOutlet weak var timerLabel: UILabel!
+	
+	var timerLabelStartSecond = 50
+	var timerLabelStartMinute = 1
+	let startDate = NSDate()
+	var timeToFinish = NSDate()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		let components = NSDateComponents()
+		components.minute = timerLabelStartMinute
+		components.second = timerLabelStartSecond
+		if let finishTime = NSCalendar.currentCalendar().dateByAddingComponents(components, toDate: startDate, options: []) {
+			timeToFinish = finishTime
+		}
+		let updateTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: #selector(ViewController.updateFunction(_:)), userInfo: nil, repeats: true)
+		NSRunLoop.currentRunLoop().addTimer(updateTimer, forMode: NSRunLoopCommonModes)
+	}
+
+	func updateFunction(sender: NSTimer)
+	{
+		let calendar = NSCalendar.currentCalendar()
+		print(calendar.components(.Second, fromDate: self.startDate, toDate: NSDate(), options: []))
+		let seconds = calendar.components(.Second, fromDate: NSDate(), toDate: self.timeToFinish, options: []).second%60
+		let minutes = calendar.components(.Minute, fromDate: NSDate(), toDate: self.timeToFinish, options: []).minute
+		timerLabel.text = "\(minutes):\(seconds)"
+	}
+	func printTimeElapsed()
+	{
+		let calendar = NSCalendar.currentCalendar()
+		print(calendar.components(.Second, fromDate: self.startDate, toDate: NSDate(), options: []))
+		delay(2) { 
+			self.printTimeElapsed()
+		}
+	}
+	func delay(delay:Double, closure:()->()) {
+		dispatch_after(
+			dispatch_time(
+				DISPATCH_TIME_NOW,
+				Int64(delay * Double(NSEC_PER_SEC))
+			),
+			dispatch_get_main_queue(), closure)
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
-
+	
 }
 
